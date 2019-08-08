16119fd36e5e803847c3e92d4c909616c1742e4e
woohs <wujun1204sc@outlook.com>
2019-08-08 11:46:27 +0800

    update: 更新md文档

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # 个人记录
 
-[1.css_01 布局练习](https://woohs.github.io/demo/quiz/css_01/)
+## css
+1. [张鑫旭小测 css布局练习](https://woohs.github.io/demo/quiz/css_01/)
+2. [BFC demo](https://woohs.github.io/demo/css/bfc/)
+3. [《CSS世界》读书笔记练习](https://woohs.github.io/demo/css/demo1/)
+
+## js
+1. [使用async模拟红绿灯](https://woohs.github.io/demo/js/async/)
+2. [张鑫旭小测 js基础测试34](https://woohs.github.io/demo/quiz/js_34/)
+
+## react
+1. [react 组件](https://woohs.github.io/demo/react/lazy-demo/build/)
---
+++ react/lazy-demo/build/precache-manifest.bb577b5e8aff0f755db9536357990ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c0d7863f1bb14de40e90938f9aeb2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e55d7a17b3023a0113",
+    "url": "/static/css/main.fb9f0fb1.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d9e55d7a17b3023a0113",
+    "url": "/static/js/main.1c1c2686.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
