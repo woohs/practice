642b34c735ea006315ece58d1fd9f7e97eaadaf2
woohs <wujun1204sc@outlook.com>
2019-11-09 14:33:12 +0800

    feat: 1.自动演示代码页面 2.懒加载图片demo

---
+++ react/lazy-demo/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    ...strictEslint.rules,
+    'no-script-url': 0,
+    'generator-star-spacing': 0,
+    'max-len': 0,
+    'react/prefer-stateless-function': 0,
+    'no-underscore-dangle': 0,
+  },
+};
